4f38ac06717c743731e85e948235a5eae22cdcd6
panykiel <paul.nykiel@gmail.com>
2017-01-10 17:54:21 +0100

    Added Script to autogenerate Tests

--- tests/gtest.cpp
+++ tests/gtest.cpp
@@ -25,6 +25,21 @@ TEST(isCircleTest, noMatchingPoints){
     }));
 }
 
+TEST(isCircleTest, isCircle){
+    EXPECT_TRUE(crclfnd::isCircle({
+           Point(50,0),
+           Point(20,20),
+           Point(0,50),
+           Point(-20,20),
+           Point(-50,0),
+           Point(-40,-10),
+           Point(-10,-40),
+           Point(0,-50),
+           Point(10,-40),
+           Point(40,-10)
+   }));
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
---
+++ tests/util/generateConturs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+int main(int argc, char** args){
+
+    std::cout << "Generating contours out of given images:" << std::endl;
+    for(int c=1; c<argc; c++){
+        std::cout << "Img: " << args[c] << std::endl;
+
+        Mat img = imread(args[c], CV_LOAD_IMAGE_GRAYSCALE);
+
+        Canny(img, img, 50, 50*3, 3);
+
+        std::vector<std::vector<Point>> contourPoints;
+        std::vector<Vec4i> hierarchy;
+
+        findContours(img, contourPoints, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+        for(int d=0; d<contourPoints.size(); d++){
+            std::cout << "{";
+            for(int e=0; e<contourPoints[d].size(); e++){
+                if(e!=0)
+                    std::cout << ", ";
+                std::cout << "Point(" << contourPoints[d][e].x << ", " << contourPoints[d][e].x <<  ")";
+            }
+            std::cout << "}" << std::endl;
+        }
+        std::cout << std::endl << std::endl;
+    }
+}
