73c587faea25a2702d6a138312c2dd9892f26006
Tim <tiluchterhand>
2017-01-12 20:54:43 +0100

    Neue Bilder + BildTests hinzugefügt

---
+++ tests/IsCircleTest/AutogeneratedImageTests/HorizontalLine.h
@@ -0,0 +1,12 @@
+TEST_TRUE(isCircleTest, HorizontalLine){
+	EXPECT_FALSE(crclfnd::isCircle(
+		{
+		Point(94,174),
+		Point(92,176),
+		Point(93,177),
+		Point(601,177),
+		Point(601,175),
+		Point(600,174)
+		}
+	).isCircle);
+}
