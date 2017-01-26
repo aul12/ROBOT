2863f9f6a814bf888955906b29d01ec8ff6a8c20
panykiel <paul.nykiel@gmail.com>
2017-01-26 23:31:16 +0100

    Fixed unit tests

--- tests/gtest.cpp
+++ tests/gtest.cpp
@@ -39,7 +39,7 @@
 
 
 int main(int argc, char **argv) {
-    dbg::init(dbg::STDOUT);
+    dbg::init(dbg::STDOUT, dbg::LOG);
       ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
